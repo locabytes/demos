ea56b15549f770994e51e00b4a8acbc778aa42fb
locabytes <mcardoso@locabytes.com.br>
2022-08-16 08:41:41 -0300

    main

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+<header>
+  <div class="navbar navbar-dark bg-dark shadow-sm">
+    <div class="container d-flex justify-content-between">
+      <a href="https://www.locabytes.com.br" class="navbar-brand d-flex align-items-center">
+            <strong>Locabytes Serviços Digitais</strong>
+      </a>
+    </div>
+  </div>
+</header>
+
+    <section class="jumbotron text-center">
+    <div class="container">
+      <h1>Veja abaixo alguns projetos desenvolvidos por nós</h1>
+      <p class="lead text-muted">Desenvolvemos sistemas em PHP, criamos seu software personalizado, criamos bibliotecas de softwares, para desenvolvimento exclusivo, faça um orçamento</p>
+      <p>
+        <a href="https://www.locabytes.com.br/orcamento" class="btn btn-primary my-2">Orçamento</a>
+      </p>
+    </div>
+    </section>
+
+    <div class="col-md-3">
+          <div class="card mb-4 shadow-sm">
+          <img src="images/qrcode.jpg" class="img-thumbnail" alt="">
+
+            <div class="card-body">
+              <p class="card-text">Script de QR-Code, para ser utilizado em projetos para geração automatica de QR-Code, geração de Chave PIX</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a href="qrcode/" target="_blank" class="btn btn-primary my-2">Demo</a>
+                  <a href="https://suporte-tecnico.mercadolivre.com.br/MLB-2758432995-script-php-gerador-de-qr-code-chave-pix-_JM" target="_blank" class="btn btn-success my-2">Comprar</a>
+                </div>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+</body>
+
+</html>

5dbb16551ddadb2b90debc9b72c362d01fd33ddc
locabytes <mcardoso@locabytes.com.br>
2022-08-16 15:23:56 -0300

    main

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt_br">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Locabytes</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"></script>
 
@@ -32,22 +32,48 @@
     </div>
     </section>
 
-    <div class="col-md-3">
-          <div class="card mb-4 shadow-sm">
-          <img src="images/qrcode.jpg" class="img-thumbnail" alt="">
 
-            <div class="card-body">
-              <p class="card-text">Script de QR-Code, para ser utilizado em projetos para geração automatica de QR-Code, geração de Chave PIX</p>
+
+  <div class="album py-5 bg-light">
+    <div class="container">
+
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div class="col">
+          <div class="card shadow-sm">
+          <img src="images/qrcode.jpg" class="img-thumbnail" width="400" alt="">  
+          <div class="card-body">
+            <p class="card-text">Script de QR-Code, para ser utilizado em projetos para geração automatica de QR-Code, geração de Chave PIX</p>
               <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
+              <div class="btn-group">
                   <a href="qrcode/" target="_blank" class="btn btn-primary my-2">Demo</a>
                   <a href="https://suporte-tecnico.mercadolivre.com.br/MLB-2758432995-script-php-gerador-de-qr-code-chave-pix-_JM" target="_blank" class="btn btn-success my-2">Comprar</a>
                 </div>
-                
+                <small class="text-muted">R$ 25,00</small>
               </div>
             </div>
           </div>
         </div>
+        <div class="col">
+          <div class="card shadow-sm">
+          <img src="images/recebepix.jpg" class="img-thumbnail" width="400" alt="">
+
+            <div class="card-body">
+              <p class="card-text">Gerador de chave pix automatica para recebimento, de forma rápida para comerciantes</p>
+              <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                  <a href="recebepix/" target="_blank" class="btn btn-primary my-2">Demo</a>
+                  <a href="https://suporte-tecnico.mercadolivre.com.br/MLB-2758432995-script-php-gerador-de-qr-code-chave-pix-_JM" target="_blank" class="btn btn-success my-2">Comprar</a>
+                </div>
+                <small class="text-muted">R$ 50,00</small>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+     </div>
+    </div>
+  </div>
 </body>
 
 </html>
